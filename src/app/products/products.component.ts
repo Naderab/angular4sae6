75110234f572c7d765a7edca2ed95dda2bbdbcb4
Naderab <abdellaouinader@gmail.com>
2022-11-11 15:03:34 +0100

    manipulation composants

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../core/product';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
   title : string = "Welcome to products list"
+  productList:Product[]=[];
   constructor() { }
 
   ngOnInit(): void {
+    this.productList = [
+      {id:'1',title:'T-shirt 1',quantity:10,price:100,like:0,picture:''},
+      {id:'2',title:'T-shirt 2',quantity:0,price:20,like:0,picture:''},
+      {id:'3',title:'T-shirt 3',quantity:10,price:50,like:0,picture:''}
+    ];
   }
 
 }
